b8e817fc21e90550faf2c081c45b3444b7b2b7a5
baypp6577 <baypp6577@gitlab.com>
2026-03-06 14:14:01 +0000

    one page

--- public/common.js
+++ public/common.js
@@ -78,7 +78,9 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('a[href^="#"]:not([href="#"])').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
-            const target = document.querySelector(this.getAttribute('href'));
+            const targetId = this.getAttribute('href');
+            const target = document.querySelector(targetId);
+            
             if (target) {
                 target.scrollIntoView({
                     behavior: 'smooth',
